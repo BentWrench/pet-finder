9910ed6a2474add864eecdbbc80d0cb1d25dbb11
POgawa & JCVogelsberg <student@epicodus.com>
2014-05-12 15:57:51 -0700

    added more capybara tests and removed styling for later unified styling

--- spec/features/pet_spec.rb
+++ spec/features/pet_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 describe "lost or found pet" do
   describe "add new" do
-    it "creates a new lost pet" do
+    it "creates a new listing" do
       visit "/pets/new"
       select("Bird", :from => "pet_species")
       fill_in "Breed", :with => "Eagle"
@@ -18,6 +18,30 @@ describe "lost or found pet" do
 
   describe "edit listing" do
     it "edits a lost pet listing" do
+      visit "/"
+      click_link "Add New Animal"
+      select("Bird", :from => "pet_species")
+      fill_in "Breed", :with => "Eagle"
+      fill_in "Color", :with => "Golden"
+      select("Downtown", :from => "pet_loc_lost")
+      choose('pet_lost_1')
+      fill_in 'Description', :with => "My eagle done flew away!"
+      click_button "Create Pet"
+      click_link "Bird"
+      click_link "Edit This Listing"
+      select("Other - Please Describe", :from => "pet_species")
+      fill_in "Breed", :with => "Gopher"
+      fill_in "Color", :with => "Yellow"
+      select("NE Portland", :from => "pet_loc_lost")
+      choose('pet_lost_1')
+      fill_in 'Description', :with => "He dug a tunnel and escaped!"
+      click_button "Update Pet"
+      page.should have_content "Pet description: He dug a tunnel and escaped!"
+    end
+  end
+
+  describe "select listing" do
+    it "selects an existing listing" do
       visit "/pets/new"
       select("Bird", :from => "pet_species")
       fill_in "Breed", :with => "Eagle"
@@ -26,10 +50,31 @@ describe "lost or found pet" do
       choose('pet_lost_1')
       fill_in 'Description', :with => "My eagle done flew away!"
       click_button "Create Pet"
-
-      page.should have_content "Pet description: My eagle done flew away!"
+      click_link "Bird"
+      page.should have_content "Reported Lost: Species: Bird"
     end
   end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
