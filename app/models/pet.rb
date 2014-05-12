b6313d1adfa1c8bc4741a6fde174a4d8b72c251f
POgawa & JCVogelsberg <student@epicodus.com>
2014-05-12 11:02:11 -0700

    initial commit with full CRUD functionality with pet model and base index view page

---
+++ app/models/pet.rb
@@ -0,0 +1,13 @@
+class Pet < ActiveRecord::Base
+
+  validates :lost_found, :presence => true
+  validates :species, :presence => true
+  validates :breed, :presence => true
+  validates :description, :presence => true
+  validates :color, :presence => true
+  validates :loc_lost, :presence => true
+
+  belongs_to :user
+
+end
+
