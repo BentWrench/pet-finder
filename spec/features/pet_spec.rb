b6694a6e9a08a4aa14d1e08775bf01e70d6514e9
POgawa & JCVogelsberg <student@epicodus.com>
2014-05-12 15:22:05 -0700

    added more capybara tests, editting and show functionality

--- spec/features/pet_spec.rb
+++ spec/features/pet_spec.rb
@@ -7,29 +7,32 @@ describe "lost or found pet" do
       select("Bird", :from => "pet_species")
       fill_in "Breed", :with => "Eagle"
       fill_in "Color", :with => "Golden"
-      select("Central", :from => "pet_loc_lost")
+      select("Downtown", :from => "pet_loc_lost")
       choose('pet_lost_1')
       fill_in 'Description', :with => "My eagle done flew away!"
       click_button "Create Pet"
       page.should have_content "Pet description: My eagle done flew away!"
     end
   end
+
+
+  describe "edit listing" do
+    it "edits a lost pet listing" do
+      visit "/pets/new"
+      select("Bird", :from => "pet_species")
+      fill_in "Breed", :with => "Eagle"
+      fill_in "Color", :with => "Golden"
+      select("Downtown", :from => "pet_loc_lost")
+      choose('pet_lost_1')
+      fill_in 'Description', :with => "My eagle done flew away!"
+      click_button "Create Pet"
+
+      page.should have_content "Pet description: My eagle done flew away!"
+    end
+  end
+
+
+
+
+
 end
-
-  # describe "view all" do
-
-
-  # end
-
-
-  # describe "" do
-  # end
-
-
-  # describe "" do
-  # end
-
-
-
-
-
