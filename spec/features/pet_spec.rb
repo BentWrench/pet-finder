2132e29f2e6dc217bc099b91de8e5d0fff963599
POgawa & JCVogelsberg <student@epicodus.com>
2014-05-13 10:34:48 -0700

    finish pet model

--- spec/features/pet_spec.rb
+++ spec/features/pet_spec.rb
@@ -6,7 +6,7 @@ describe "lost or found pet" do
       visit "/pets/new"
       select("Bird", :from => "pet_species")
       fill_in "Breed", :with => "Eagle"
-      fill_in "Color", :with => "Golden"
+      select("Brown", :from => "pet_color")
       select("Downtown", :from => "pet_loc_lost")
       choose('pet_lost_1')
       fill_in 'Description', :with => "My eagle done flew away!"
@@ -19,24 +19,29 @@ describe "lost or found pet" do
   describe "edit listing" do
     it "edits a lost pet listing" do
       visit "/"
-      click_link "Add New Animal"
+      click_link "Sign up"
+      fill_in "Email", :with => "test@test.com"
+      fill_in "Password", :with => "12345678"
+      fill_in "Password confirmation", :with => "12345678"
+      click_button "Sign up"
+      visit "/pets/new"
       select("Bird", :from => "pet_species")
       fill_in "Breed", :with => "Eagle"
-      fill_in "Color", :with => "Golden"
+      select("Brown", :from => "pet_color")
       select("Downtown", :from => "pet_loc_lost")
       choose('pet_lost_1')
-      fill_in 'Description', :with => "My eagle done flew away!"
+      fill_in 'Description', :with => "eagle eagle eagle"
       click_button "Create Pet"
       click_link "Bird"
       click_link "Edit This Listing"
       select("Other - Please Describe", :from => "pet_species")
       fill_in "Breed", :with => "Gopher"
-      fill_in "Color", :with => "Yellow"
+      select("Black", :from => "pet_color")
       select("NE Portland", :from => "pet_loc_lost")
       choose('pet_lost_1')
-      fill_in 'Description', :with => "He dug a tunnel and escaped!"
+      fill_in 'Description', :with => "gopher gopher gopher"
       click_button "Update Pet"
-      page.should have_content "Pet description: He dug a tunnel and escaped!"
+      page.should have_content "Pet description: gopher gopher gopher"
     end
   end
 
@@ -45,36 +50,15 @@ describe "lost or found pet" do
       visit "/pets/new"
       select("Bird", :from => "pet_species")
       fill_in "Breed", :with => "Eagle"
-      fill_in "Color", :with => "Golden"
+      select("Brown", :from => "pet_color")
       select("Downtown", :from => "pet_loc_lost")
       choose('pet_lost_1')
-      fill_in 'Description', :with => "My eagle done flew away!"
+      fill_in 'Description', :with => "eagle eagle eagle"
       click_button "Create Pet"
       click_link "Bird"
       page.should have_content "Reported Lost: Species: Bird"
     end
   end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
