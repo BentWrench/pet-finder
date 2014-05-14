e50881985b9ee434ede2ba02905f28438bddb056
Patrick McGreevy <patrick7490@icloud.com>
2014-05-14 14:06:48 -0700

    add some tests

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,8 +1,7 @@
 class Pet < ActiveRecord::Base
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/No_image_available.png"
 
-validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
-
+  validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
   validates_inclusion_of :lost, :in => [true, false]
   validates :species, :presence => true
   validates :breed, :presence => true
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,7 @@
 require 'spec_helper'
 
 describe User do
+  it { should validate_presence_of :role }
+  it { should ensure_inclusion_of(:role).in_array(User::ROLES) }
+  it { should have_many :pets }
 end
