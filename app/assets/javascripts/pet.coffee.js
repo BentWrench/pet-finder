c84b8691009467323ed1de2bad61567fa55646c9
Paul Ogawa <student@epicodus.com>
2014-05-15 15:01:21 -0700

    added more specificity to the form and toggles lost or found

--- app/assets/javascripts/pet.coffee.js
+++ app/assets/javascripts/pet.coffee.js
@@ -1,1 +1,16 @@
-pet.coffee.js
+# Place all the behaviors and hooks related to the matching controller here.
+# All this logic will automatically be available in application.js.
+# You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+jQuery ->
+  $('#pet_state_id').parent().hide()
+  states = $('#pet_state_id').html()
+  $('#pet_species_id').change ->
+    country = $('#pet_species_id :selected').text()
+    escaped_country = country.replace(/([ #;&,.+*~\':"!^$[\]()=>|\/@])/g, '\\$1')
+    options = $(states).filter("optgroup[label='#{escaped_country}']").html()
+    if options
+      $('#pet_state_id').html(options)
+      $('#pet_state_id').parent().show()
+    else
+      $('#pet_state_id').empty()
+      $('#pet_state_id').parent().hide()
