86e2a7338f9e642607d8dbd00aa3a91cbc6f95fd
Paul Ogawa <student@epicodus.com>
2014-05-16 14:27:46 -0700

    aded to git ingmore file on front end branch

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,9 @@
+class UserMailer < ActionMailer::Base
+  default from: 'notifications@example.com'
+
+  def welcome_email(user)
+    @user = user
+    @url  = 'http://example.com/login'
+    mail(to: @user.email, subject: 'Thank you for signing up for the Oregon Humane Societies pet tracker')
+  end
+end
