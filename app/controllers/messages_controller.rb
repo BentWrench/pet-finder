38450699bd0c5573bb1f19618a80634c944344b4
JCVogelsberg <student@epicodus.com>
2014-05-23 15:56:31 -0700

    emailing to lost pet owner vs. pet finder straightened out

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,18 +6,14 @@ class MessagesController < ApplicationController
 
   def create
     @message = Message.new(params)
-    if params[:lost]
+    if (params[:lost]) == "true"
       UserMailer.found_pet_email(@message).deliver
       flash[:notice] = "An email to the pet owner has been sent!"
       redirect_to pets_path
     else
       UserMailer.lost_pet_email(@message).deliver
-      flash[:notice] = "An email to the finder of this pet has been sent!"
+      flash[:notice] = "An email has been sent to the finder of this pet!"
       redirect_to pets_path
     end
   end
 end
-
-
-
-
