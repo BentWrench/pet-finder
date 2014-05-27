58935965e916a9d62175ecdc18140afc5d170d6d
Patrick McGreevy & Paul Ogawa <patrick7490@icloud.com>
2014-05-27 13:59:39 -0700

    re-format code, refactor all pet listing rendering

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -17,7 +17,7 @@ class Pet < ActiveRecord::Base
                     :styles => { :large => "600x600>",
                                   :medium => "300x300>",
                                   :thumb => "100x100>" },
-                                  :default_url => "/images/:style/No_image_available.png"
+                    :default_url => "/images/:style/No_image_available.png"
 
   validates_attachment_content_type :avatar,
                                     :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
