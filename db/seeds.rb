8a8bffa149c5cc06f7d91b2bfd5579ded400f3f8
Patrick McGreevy <patrick7490@icloud.com>
2014-06-04 12:04:40 -0700

    change seeds to real email

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,9 @@ Pet::COLORS.each do |color|
   Color.create(name: color)
 end
 
-ADMIN = User.create(email: 'admin@test.com', password: 'password', role: 'admin')
-USER = User.create(email: 'user@test.com', password: 'password')
+ADMIN = User.create(email: '101eb27e@opayq.com', password: 'password', role: 'admin')
+# User.create(email: 'admin@test.com', password: 'password', role: 'admin')
+# USER = User.create(email: 'user@test.com', password: 'password')
 
 Pet.create(lost: true, user: ADMIN, species: 'Reptile', breed: 'Iguana',
   description: 'Lost iguana listing', colors: Color.where(name: ['Green', 'Black']), loc_lost: 'Downtown')
